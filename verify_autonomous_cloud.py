6ac79dd90437161efa7d633053907980d9217cc0
Quant Trader <trader@binancequant.internal>
2026-07-29 11:39:56 -0500

    fix: Use correct ventoux Fixie proxy URL to resolve 407 auth error

--- verify_autonomous_cloud.py
+++ verify_autonomous_cloud.py
@@ -9,7 +9,7 @@ API_KEY = os.getenv("BINANCE_REAL_API_KEY", "")
 API_SECRET = os.getenv("BINANCE_REAL_API_SECRET", "")
 PROXY_URL = os.getenv("FIXIE_URL", "")
 if not PROXY_URL:
-    PROXY_URL = "http://fixie:XqF868E4rA6L6Gj@velodrome.usefixie.com:80"
+    PROXY_URL = "http://fixie:yqYN8TxTpLkrqC0@ventoux.usefixie.com:80"
 
 PROXIES = {
     "http": PROXY_URL,
